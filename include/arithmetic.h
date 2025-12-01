7fff8b1f3923813730a8e98b34e80972ad0ff0ae
aliska88 <aliska_88k@mail.ru>
2025-12-02 00:41:02 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,36 @@
-// объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#ifndef POSTFIX_H
+#define POSTFIX_H
+
+#include <string>
+#include "lexemes.h"
+#include "stack.h"
+
+class Postfix {
+ public:
+  Postfix();
+  ~Postfix();
+
+  void set_infix(std::string infix);
+  std::string get_postfix_string();
+
+  void parse();
+  void to_postfix();
+  double calculate();
+
+ private:
+  std::string infix_;
+
+  Token** infixTokens_;
+  int infixLen_;
+  int infixMaxLen_;
+
+  Token** postfixTokens_;
+  int postfixLen_;
+  int postfixMaxLen_;
+
+  void add_token(Token**& arr, int& len, int& maxLen, Token* item);
+
+  void clean();
+};
+
+#endif
