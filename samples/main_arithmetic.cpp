d08c9746a058635970337fc0258e5466250d9d81
aliska88 <aliska_88k@mail.ru>
2025-12-02 10:07:47 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,24 @@
-// реализация пользовательского приложения
+#include <iostream>
 
-int main()
-{
+#include "arithmetic.h"
+
+int main() {
+  Postfix calculator;
+  std::string expr;
+
+  std::cout << "Enter expression: ";
+  std::getline(std::cin, expr);
+
+  try {
+    calculator.set_infix(expr);
+    calculator.parse();
+    calculator.to_postfix();
+
+    std::cout << "Postfix: " << calculator.get_postfix_string() << std::endl;
+    std::cout << "Result: " << calculator.calculate() << std::endl;
+  } catch (const std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+
   return 0;
 }
